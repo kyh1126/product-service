113b526563135d2ab66107cf6c068b6e726d0a41
jenny <yunhee.kim@smartfoodnet.com>
2021-09-08 18:28:06 +0900

    feat: 스프링부트의 자동설정을 유지하는 Web Configuration 추가 (+ 스웨거 UI 경로 추가)

---
+++ src/main/kotlin/com/smartfoodnet/config/WebMvcConfig.kt
@@ -0,0 +1,14 @@
+package com.smartfoodnet.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+            .resourceChain(false)
+    }
+}
