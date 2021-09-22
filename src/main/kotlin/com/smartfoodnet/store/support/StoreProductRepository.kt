892c483d3af6d2b5abb79b75f04d74e3bafa7487
woo.noh <wooyoung.noh@smartfoodnet.com>
2021-09-23 01:48:30 +0900

    feat: store product basic functions created

--- src/main/kotlin/com/smartfoodnet/store/support/StoreProductRepository.kt
+++ src/main/kotlin/com/smartfoodnet/store/support/StoreProductRepository.kt
@@ -4,5 +4,5 @@ import com.smartfoodnet.store.entity.StoreProduct
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface StoreProductRepository : JpaRepository<StoreProduct, Long> {
-
+    fun findAllByPartnerId(partnerId: Long): List<StoreProduct>
 }
