aac678fc518bb808cfac44f3a7b0edaac1ab7550
jenny <yunhee.kim@smartfoodnet.com>
2021-09-23 11:26:15 +0900

    dev: @ResponseBody 실행 또는 ResponseEntity 컨트롤러 메서드를 실행한 후, HttpMessageConverter 로 body 를 쓰기전에 응답 커스터마이징 추가

---
+++ src/main/kotlin/com/smartfoodnet/common/model/ResponseHandler.kt
@@ -0,0 +1,33 @@
+package com.smartfoodnet.common.model
+
+import com.smartfoodnet.common.error.ExceptionResponse
+import com.smartfoodnet.common.model.response.CommonResponse
+import org.springframework.core.MethodParameter
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+
+@RestControllerAdvice
+class ResponseHandler : ResponseBodyAdvice<Any> {
+    override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>>): Boolean {
+        return true
+    }
+
+    override fun beforeBodyWrite(
+        body: Any?,
+        returnType: MethodParameter,
+        selectedContentType: MediaType,
+        selectedConverterType: Class<out HttpMessageConverter<*>>,
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+    ): Any? {
+        return if (body!! is ExceptionResponse || request.uri.path.contains("swagger") || request.uri.path.contains("api-docs")) {
+            body
+        } else {
+            CommonResponse(payload = body)
+        }
+    }
+}
