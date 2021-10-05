a52b412041b4243295c272bfdb05b47be8c224ed
jenny <yunhee.kim@smartfoodnet.com>
2021-10-05 16:54:59 +0900

    issue/fse638: Time 관련 상수 object 추가

---
+++ src/main/kotlin/com/smartfoodnet/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.smartfoodnet.common
+
+object Constants {
+    const val TIMESTAMP3_FORMAT = "yyyy-MM-dd HH:mm:ss:SSS"
+    const val TIMESTAMP_MINUTE_FORMAT = "yyyy-MM-dd HH:mm"
+    const val DATE_FORMAT = "yyyy-MM-dd"
+    const val TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss"
+    const val TIMEZONE = "Asia/Seoul"
+}
