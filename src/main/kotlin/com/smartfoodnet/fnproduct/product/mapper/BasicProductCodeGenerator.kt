58ec597d33a746a813166947bf6827d11bdbb1f7
jenny <yunhee.kim@smartfoodnet.com>
2021-10-14 19:44:26 +0900

    issue/fse638: 상품코드 채번 generator 에서 기본상품 type 이 채번 가능한 타입인지 체크 추가

--- src/main/kotlin/com/smartfoodnet/fnproduct/product/mapper/BasicProductCodeGenerator.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/product/mapper/BasicProductCodeGenerator.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 /**
- * 상품코드 채번 규칙<p>
- *
+ * 상품코드 채번 규칙
+ * <p>
  * @see <a href="https://docs.google.com/spreadsheets/d/1z1vAZCbcleMM0v5nDbJl-aER0ExCgMx_50-obAxsd0c/edit#gid=1591663723">상품코드채번규칙</a>
  */
 @Component
@@ -17,18 +17,22 @@ class BasicProductCodeGenerator(
     private val partnerService: PartnerService,
     private val basicProductRepository: BasicProductRepository,
 ) {
+    private val basicProductCodeTypes = setOf(BasicProductType.BASIC, BasicProductType.PACKAGE)
+
     fun getBasicProductCode(
         partnerId: Long,
-        typeCode: String?,
+        type: BasicProductType,
         temperatureCode: String?,
-    ): String {
-        val customerNumber = partnerService.getPartner(partnerId).customerNumber
-        val basicProductCodeTypes = setOf(BasicProductType.BASIC, BasicProductType.PACKAGE)
+    ): String? {
+        if (validateNotAvailableType(type)) return null
 
+        val customerNumber = partnerService.getPartner(partnerId).customerNumber
         // 00001 부터 시작
         val totalProductCount = basicProductRepository.countByPartnerIdAndTypeIn(partnerId, basicProductCodeTypes)
             .run { String.format("%05d", this + 1) }
 
-        return customerNumber + typeCode + temperatureCode + totalProductCount
+        return customerNumber + type.code + temperatureCode + totalProductCount
     }
+
+    private fun validateNotAvailableType(type: BasicProductType?) = type !in basicProductCodeTypes
 }
