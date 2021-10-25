6c72de17d8cdd9907855be209060a8b7e42f8a02
jenny <yunhee.kim@smartfoodnet.com>
2021-10-25 18:08:49 +0900

    issue/fse584: 모음상품 테이블 및 엔티티 설계

---
+++ src/main/kotlin/com/smartfoodnet/fnproduct/product/entity/PackageProduct.kt
@@ -0,0 +1,26 @@
+package com.smartfoodnet.fnproduct.product.entity
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.smartfoodnet.common.entity.BaseEntity
+import javax.persistence.*
+
+@Entity
+class PackageProduct(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
+    var id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "package_product_id", columnDefinition = "BIGINT UNSIGNED")
+    @JsonIgnore
+    var packageProduct: BasicProduct? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "basic_product_id", columnDefinition = "BIGINT UNSIGNED")
+    @JsonIgnore
+    var basicProduct: BasicProduct,
+
+    @Column(name = "quantity")
+    var quantity: Int,
+) : BaseEntity()
