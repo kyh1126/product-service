a2940bd5d9cb84a662950860e645feb0a4978a69
jenny <yunhee.kim@smartfoodnet.com>
2021-10-26 16:34:57 +0900

    issue/fse584: 모음상품정보(매핑) 엔티티 내 update, delete 메소드 추가

--- src/main/kotlin/com/smartfoodnet/fnproduct/product/entity/PackageProduct.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/product/entity/PackageProduct.kt
@@ -2,9 +2,12 @@ package com.smartfoodnet.fnproduct.product.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.smartfoodnet.common.entity.BaseEntity
+import com.smartfoodnet.fnproduct.product.model.request.PackageProductCreateModel
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
+@Table(name = "package_product")
 class PackageProduct(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +26,13 @@ class PackageProduct(
 
     @Column(name = "quantity")
     var quantity: Int,
-) : BaseEntity()
+) : BaseEntity() {
+    fun update(request: PackageProductCreateModel, basicProductPackage: BasicProduct) {
+        packageProduct = basicProductPackage
+        quantity = request.quantity
+    }
+
+    fun delete() {
+        deletedAt = LocalDateTime.now()
+    }
+}
