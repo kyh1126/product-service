b18033274d1f98ea31c1bf2b9d1e74ca15ad0aef
jenny <yunhee.kim@smartfoodnet.com>
2021-10-26 15:42:54 +0900

    issue/fse584: 모음상품 추가 api 유효성 체크 Validator 추가

---
+++ src/main/kotlin/com/smartfoodnet/fnproduct/product/validator/PackageProductDetailCreateModelValidator.kt
@@ -0,0 +1,58 @@
+package com.smartfoodnet.fnproduct.product.validator
+
+import com.smartfoodnet.common.error.CreateModelValidator
+import com.smartfoodnet.common.error.SaveState
+import com.smartfoodnet.fnproduct.product.model.request.PackageProductDetailCreateModel
+import com.smartfoodnet.fnproduct.product.model.vo.BasicProductType
+import org.springframework.stereotype.Component
+import org.springframework.validation.Errors
+
+@Component
+class PackageProductDetailCreateModelValidator(
+    private val basicProductSimpleCreateModelValidator: BasicProductSimpleCreateModelValidator
+) : CreateModelValidator<PackageProductDetailCreateModel> {
+    override fun supports(clazz: Class<*>): Boolean =
+        clazz.isAssignableFrom(PackageProductDetailCreateModel::class.java)
+
+    override fun validate(
+        saveState: SaveState,
+        target: PackageProductDetailCreateModel,
+        errors: Errors
+    ) {
+        checkRequiredFields(saveState, target, errors)
+    }
+
+    private fun checkRequiredFields(
+        saveState: SaveState,
+        target: PackageProductDetailCreateModel,
+        errors: Errors,
+    ) {
+        val basicProductModel = target.basicProductModel
+        val packageProductModels = target.packageProductModels
+
+        with(basicProductModel) {
+            if (type != BasicProductType.PACKAGE) {
+                errors.rejectValue(
+                    "basicProductModel.type",
+                    "type.invalid",
+                    "모음상품 타입만(PACKAGE) 가능합니다."
+                )
+            }
+
+            validateNull(errors, "basicProductModel.partnerId", "화주(고객사) ID", partnerId)
+            validateEmpty(errors, "basicProductModel.name", "상품명", name)
+            validateEmpty(errors, "basicProductModel.barcodeYn", "상품바코드기재여부", barcodeYn)
+        }
+
+        validateEmpty(errors, "packageProductModels", "모음상품(매핑)정보", packageProductModels)
+        if (packageProductModels.isNotEmpty()) {
+            validateCollection(
+                saveState,
+                errors,
+                "packageProductModels",
+                packageProductModels.map { it.packageProduct },
+                basicProductSimpleCreateModelValidator
+            )
+        }
+    }
+}
