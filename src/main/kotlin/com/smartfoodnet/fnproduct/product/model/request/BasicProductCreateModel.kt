382ccb74b0b869836990d64cb8eab012da83853a
jenny <yunhee.kim@smartfoodnet.com>
2021-12-07 18:52:34 +0900

    issue/fse683: Spring validation 이슈로 모델을 본문으로 쓰도록 변경 (https://thesfn.slack.com/archives/C02062ZSG6R/p1638867522008400)

--- src/main/kotlin/com/smartfoodnet/fnproduct/product/model/request/BasicProductCreateModel.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/product/model/request/BasicProductCreateModel.kt
@@ -10,69 +10,69 @@ import com.smartfoodnet.fnproduct.warehouse.entity.InWarehouse
 import io.swagger.annotations.ApiModelProperty
 import javax.validation.constraints.NotNull
 
-data class BasicProductCreateModel(
+class BasicProductCreateModel {
     @ApiModelProperty(value = "id")
-    val id: Long? = null,
+    val id: Long? = null
 
     @NotNull
     @ApiModelProperty(
         value = "구분 (BASIC:기본상품/CUSTOM_SUB:고객전용부자재/SUB:공통부자재/PACKAGE:모음상품)",
         allowableValues = "BASIC,CUSTOM_SUB,SUB,PACKAGE", dataType = "Enum", example = "BASIC"
     )
-    val type: BasicProductType = BasicProductType.BASIC,
+    val type: BasicProductType = BasicProductType.BASIC
 
     @ApiModelProperty(value = "화주(고객사) ID", example = "1")
-    val partnerId: Long? = null,
+    val partnerId: Long? = null
 
     @ApiModelProperty(value = "상품명")
-    val name: String? = null,
+    val name: String? = null
 
     @ApiModelProperty(value = "상품코드")
-    val code: String? = null,
+    val code: String? = null
 
     @ApiModelProperty(value = "상품바코드기재여부", allowableValues = "Y,N")
-    val barcodeYn: String = "N",
+    val barcodeYn: String = "N"
 
     @ApiModelProperty(value = "상품바코드")
-    val barcode: String? = null,
+    val barcode: String? = null
 
     @ApiModelProperty(value = "상품카테고리 ID")
-    val basicProductCategoryId: Long? = null,
+    val basicProductCategoryId: Long? = null
 
     @ApiModelProperty(value = "부자재카테고리 ID")
-    val subsidiaryMaterialCategoryId: Long? = null,
+    val subsidiaryMaterialCategoryId: Long? = null
 
     @ApiModelProperty(
         value = "취급온도 (ROOM:상온/REFRIGERATE:냉장/FREEZE:냉동)",
         allowableValues = "ROOM,REFRIGERATE,FREEZE"
     )
-    val handlingTemperature: HandlingTemperatureType? = null,
+    val handlingTemperature: HandlingTemperatureType? = null
 
     @NotNull
     @ApiModelProperty(value = "입고처 ID")
-    val warehouseId: Long,
+    val warehouseId: Long? = null
 
     @ApiModelProperty(value = "공급가")
-    val supplyPrice: Int? = null,
+    val supplyPrice: Int? = null
 
     @ApiModelProperty(value = "단수(포장)여부", allowableValues = "Y,N")
-    val singlePackagingYn: String = "N",
+    val singlePackagingYn: String = "N"
 
     @ApiModelProperty(value = "유통기한관리여부 (default: N)", allowableValues = "Y,N")
-    val expirationDateManagementYn: String = "N",
+    val expirationDateManagementYn: String = "N"
 
     @ApiModelProperty(value = "박스입수")
-    val piecesPerBox: Int? = null,
+    val piecesPerBox: Int? = null
 
     @ApiModelProperty(value = "파레트입수")
-    val boxesPerPalette: Int? = null,
+    val boxesPerPalette: Int? = null
 
     @ApiModelProperty(value = "상품이미지 URL")
-    val imageUrl: String? = null,
+    val imageUrl: String? = null
 
     @ApiModelProperty(value = "활성화여부 (default: N)", allowableValues = "Y,N")
-    val activeYn: String = "N",
-) {
+    val activeYn: String = "N"
+
     @JsonUnwrapped
     var expirationDateInfoModel: ExpirationDateInfoCreateModel? = null
 
--- src/main/kotlin/com/smartfoodnet/fnproduct/product/model/request/BasicProductSimpleCreateModel.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/product/model/request/BasicProductSimpleCreateModel.kt
@@ -6,44 +6,44 @@ import com.smartfoodnet.fnproduct.product.model.vo.HandlingTemperatureType
 import io.swagger.annotations.ApiModelProperty
 import javax.validation.constraints.NotNull
 
-data class BasicProductSimpleCreateModel(
+class BasicProductSimpleCreateModel {
     @ApiModelProperty(value = "id")
-    val id: Long? = null,
+    val id: Long? = null
 
     @NotNull
     @ApiModelProperty(
         value = "구분 (BASIC:기본상품/CUSTOM_SUB:고객전용부자재/SUB:공통부자재/PACKAGE:모음상품)",
         allowableValues = "BASIC,CUSTOM_SUB,SUB,PACKAGE"
     )
-    val type: BasicProductType,
+    val type: BasicProductType? = null
 
     @ApiModelProperty(value = "화주(고객사) ID")
-    val partnerId: Long? = null,
+    val partnerId: Long? = null
 
     @ApiModelProperty(value = "상품명")
-    val name: String? = null,
+    val name: String? = null
 
     @ApiModelProperty(value = "상품코드")
-    val code: String? = null,
+    val code: String? = null
 
     @ApiModelProperty(value = "상품바코드기재여부", allowableValues = "Y,N")
-    val barcodeYn: String = "N",
+    val barcodeYn: String = "N"
 
     @ApiModelProperty(value = "상품바코드")
-    val barcode: String? = null,
+    val barcode: String? = null
 
     @ApiModelProperty(
         value = "취급온도 (ROOM:상온/REFRIGERATE:냉장/FREEZE:냉동/MIX:혼합)",
         allowableValues = "ROOM,REFRIGERATE,FREEZE,MIX"
     )
-    val handlingTemperature: HandlingTemperatureType? = null,
+    val handlingTemperature: HandlingTemperatureType? = null
 
     @ApiModelProperty(value = "활성화여부 (default: N)", allowableValues = "Y,N")
-    val activeYn: String = "N",
-) {
+    val activeYn: String = "N"
+
     fun toEntity(code: String?): BasicProduct {
         return BasicProduct(
-            type = type,
+            type = type!!,
             partnerId = partnerId,
             name = name,
             code = code,
