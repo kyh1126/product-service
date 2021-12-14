77e117486d38d0b779a02a3d41476ce0ef955130
woo.noh <wooyoung.noh@smartfoodnet.com>
2021-12-14 15:15:35 +0900

    fse-808: getOrderDetail with condition added for testing

--- src/main/kotlin/com/smartfoodnet/fnproduct/order/support/OrderDetailRepository.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/order/support/OrderDetailRepository.kt
@@ -2,6 +2,7 @@ package com.smartfoodnet.fnproduct.order.support
 
 import com.smartfoodnet.fnproduct.order.entity.OrderDetail
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor
 
-interface OrderDetailRepository: JpaRepository<OrderDetail, Long> {
+interface OrderDetailRepository: JpaRepository<OrderDetail, Long>, QuerydslPredicateExecutor<OrderDetail> {
 }
