b3886070709f2f6643b4989e22c8ba3be5347b15
jenny <yunhee.kim@smartfoodnet.com>
2021-12-23 18:37:19 +0900

    feat/FSE-737: fn-message 로 통신할 수 있도록 추상클래스 리팩토링 후 Message Api 클라이언트 추가

---
+++ src/main/kotlin/com/smartfoodnet/apiclient/MessageApiClient.kt
@@ -0,0 +1,53 @@
+package com.smartfoodnet.apiclient
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.smartfoodnet.apiclient.SfnService.FN_PRODUCT
+import com.smartfoodnet.apiclient.model.SfnMessage
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.RestTemplate
+
+@Component
+//@Async("taskExecutor") // 비동기로 메시지 send 할 때 설정
+@Transactional
+class MessageApiClient(
+    @Value("\${sfn.service.fn-message}") override val host: String,
+    override val restTemplate: RestTemplate,
+) : RestTemplateClient(host, restTemplate) {
+
+    fun sendMessage(destination: SfnTopic, subject: String? = null, message: Any): String? {
+        return post(
+            "/messages/sns?destination=$destination",
+            SfnMessage(subject, jsonProcessedBody(message))
+        )
+    }
+
+    fun sendMessage(destination: SfnQueue, subject: String? = null, message: Any): String? {
+        return post(
+            "/messages/sqs?destination=$destination",
+            SfnMessage(subject, jsonProcessedBody(message))
+        )
+    }
+
+    private fun jsonProcessedBody(message: Any): String {
+        try {
+            return objectMapper.writeValueAsString(message)
+        } catch (e: JsonProcessingException) {
+            log.error("[MessageApiClient] 잘못된 메시지 형식입니다. (message: $message)", e)
+            throw IllegalArgumentException("Format $message is illegal")
+        }
+    }
+}
+
+enum class SfnService {
+    FN_PRODUCT
+}
+
+enum class SfnQueue(val service: SfnService) {
+    BASIC_PRODUCT_CREATED(FN_PRODUCT)
+}
+
+enum class SfnTopic {
+    PRODUCT_CREATED
+}
