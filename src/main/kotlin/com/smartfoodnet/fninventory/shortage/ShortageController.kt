d56e0f64dff98d7237992a100d5c624ef0e71788
woo.noh <wooyoung.noh@smartfoodnet.com>
2021-12-28 15:36:08 +0900

    fse-828: 결품현황 컨트롤러 추가

--- src/main/kotlin/com/smartfoodnet/fninventory/shortage/ShortageController.kt
+++ src/main/kotlin/com/smartfoodnet/fninventory/shortage/ShortageController.kt
@@ -1,6 +1,11 @@
 package com.smartfoodnet.fninventory.shortage
 
+import com.smartfoodnet.fninventory.shortage.model.ProductShortageModel
 import io.swagger.annotations.Api
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("shortage")
 class ShortageController (private val shortageService: ShortageService){
+    @Operation(summary = "특정 화주(고객사) ID 의 결품현황 리스트 조회")
+    @GetMapping("partners/{partnerId}")
+    fun getProductShortages(
+        @Parameter(description = "화주(고객사) ID", required = true)
+        @PathVariable partnerId: Long,
+    ): List<ProductShortageModel> {
+        return shortageService.getProductShortages(partnerId)
+    }
 }
