da28fda9ba86062383e4898080ceb22f5f83d96b
jenny <yunhee.kim@smartfoodnet.com>
2021-12-29 15:23:13 +0900

    dev: 누락된 import 추가

--- src/main/kotlin/com/smartfoodnet/fnproduct/consumer/ProductListener.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/consumer/ProductListener.kt
@@ -5,9 +5,8 @@ import com.smartfoodnet.common.utils.Log
 import com.smartfoodnet.config.aws.MessageAttribute.APPROXIMATE_FIRST_RECEIVE_TIMESTAMP
 import com.smartfoodnet.config.aws.MessageAttribute.MESSAGE_GROUP_ID
 import io.awspring.cloud.messaging.listener.Acknowledgment
-import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy
-import io.awspring.cloud.messaging.listener.annotation.SqsListener
 import org.springframework.messaging.handler.annotation.Headers
+import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
