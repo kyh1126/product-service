4b1ad36ef51255881cc843f1e31e352b98f43899
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2022-02-25 10:29:14 +0900

    feat: 입고예정등록 반환값 추가(createdAt)

--- src/main/kotlin/com/smartfoodnet/fninventory/inbound/model/dto/CreateInboundResponse.kt
+++ src/main/kotlin/com/smartfoodnet/fninventory/inbound/model/dto/CreateInboundResponse.kt
@@ -1,6 +1,12 @@
 package com.smartfoodnet.fninventory.inbound.model.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.smartfoodnet.common.Constants
+import java.time.LocalDateTime
+
 data class CreateInboundResponse (
     val id: Long,
-    val partnerId: Long
+    val partnerId: Long,
+    @JsonFormat(pattern = Constants.TIMESTAMP_FORMAT)
+    val createdAt: LocalDateTime?
 )
