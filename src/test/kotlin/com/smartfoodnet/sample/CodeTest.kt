6ca79f52dcf5793cf0886c37b4408537b58b7010
woo.noh <wooyoung.noh@smartfoodnet.com>
2022-03-03 15:08:45 +0900

    fix: added env value on test yml file

--- src/test/kotlin/com/smartfoodnet/sample/CodeTest.kt
+++ src/test/kotlin/com/smartfoodnet/sample/CodeTest.kt
@@ -1,34 +1,10 @@
 package com.smartfoodnet.sample
 
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.scheduling.annotation.Async
-import org.springframework.stereotype.Component
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("local")
 class CodeTest {
 
-    @Autowired
-    lateinit var a: Sample
-
-    @Test
-    fun test() {
-        println("Before, in test: " + Thread.currentThread())
-        for (i in 0..10) {
-            a.async()
-        }
-        println("After, in test: " + Thread.currentThread())
-    }
-}
-
-@Component
-class Sample {
-    @Async("taskExecutor")
-    fun async() {
-        println("In async: " + Thread.currentThread())
-        Thread.sleep(3000)
-    }
 }
