bb78d5f14ec0328f789a97c932570880eed5d86f
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2022-03-08 16:20:53 +0900

    feat: 주문수집 중복 데이터 처리 추가

--- src/main/kotlin/com/smartfoodnet/fnproduct/order/support/CollectedOrderRepository.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/order/support/CollectedOrderRepository.kt
@@ -2,10 +2,12 @@ package com.smartfoodnet.fnproduct.order.support
 
 import com.smartfoodnet.fnproduct.order.entity.CollectedOrder
 import com.smartfoodnet.fnproduct.order.model.OrderStatus
+import org.hibernate.mapping.UniqueKey
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 
 interface CollectedOrderRepository : JpaRepository<CollectedOrder, Long>,
     QuerydslPredicateExecutor<CollectedOrder>, CollectedOrderRepositoryCustom{
     fun findAllByPartnerIdAndStatus(partnerId: Long, status: OrderStatus): List<CollectedOrder>?
+    fun existsByOrderUniqueKey(uniqueKey: String) : Boolean
 }
