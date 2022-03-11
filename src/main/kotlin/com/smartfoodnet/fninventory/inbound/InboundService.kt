1027447dd6b1d823057982d53e777aed8345e327
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2022-03-11 18:25:06 +0900

    feat: 입고상세조회 응답 모델 변경

--- src/main/kotlin/com/smartfoodnet/fninventory/inbound/InboundService.kt
+++ src/main/kotlin/com/smartfoodnet/fninventory/inbound/InboundService.kt
@@ -3,8 +3,10 @@ package com.smartfoodnet.fninventory.inbound
 import com.smartfoodnet.common.error.exception.BaseRuntimeException
 import com.smartfoodnet.common.model.response.PageResponse
 import com.smartfoodnet.common.utils.Log
+import com.smartfoodnet.fninventory.inbound.entity.InboundExpectedDetail
 import com.smartfoodnet.fninventory.inbound.model.dto.CreateInboundResponse
 import com.smartfoodnet.fninventory.inbound.model.dto.GetInbound
+import com.smartfoodnet.fninventory.inbound.model.dto.GetInboundDetailModel
 import com.smartfoodnet.fninventory.inbound.model.request.InboundCreateModel
 import com.smartfoodnet.fninventory.inbound.model.request.InboundSearchCondition
 import com.smartfoodnet.fninventory.inbound.model.vo.InboundStatusType
@@ -18,12 +20,13 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class InboundService(
     val inboundRepository: InboundRepository,
+    val inboundExpectedDetailService: InboundExpectedDetailService,
     val basicProductRepository: BasicProductRepository,
     val nosnosClientService: NosnosClientService
 ) : Log {
 
     @Transactional
-    fun createInbound(createModel: InboundCreateModel) : CreateInboundResponse {
+    fun createInbound(createModel: InboundCreateModel): CreateInboundResponse {
         val inbound = createModel.toEntity()
         createModel.expectedList.forEach {
             val basicProduct = getBasicProduct(it.basicProductId)
@@ -36,17 +39,18 @@ class InboundService(
         return CreateInboundResponse(response.id!!, response.partnerId, response.createdAt)
     }
 
-    private fun getBasicProduct(basicProductCode : String) : BasicProduct {
-        return basicProductRepository.findByCode(basicProductCode)?:throw NoSuchElementException("기본상품(${basicProductCode})을 찾을 수 없습니다.")
+    private fun getBasicProduct(basicProductCode: String): BasicProduct {
+        return basicProductRepository.findByCode(basicProductCode)
+            ?: throw NoSuchElementException("기본상품(${basicProductCode})을 찾을 수 없습니다.")
     }
 
-    private fun getBasicProduct(basicProductId : Long) : BasicProduct {
+    private fun getBasicProduct(basicProductId: Long): BasicProduct {
         return basicProductRepository.findById(basicProductId).get()
     }
 
-    fun getInbound(condition: InboundSearchCondition, page: Pageable) : PageResponse<GetInbound>?{
+    fun getInbound(condition: InboundSearchCondition, page: Pageable): PageResponse<GetInbound>? {
         val parent = inboundRepository.findInbounds(condition, page)
-        var list : List<GetInbound> = listOf()
+        var list: List<GetInbound> = listOf()
         if (parent.content.size > 0) {
             val actualDetail =
                 inboundRepository.findSumActualDetail(parent.content.map { it.inboundExpectedId })
@@ -66,16 +70,21 @@ class InboundService(
         )
     }
 
-    fun getInboundActualDetail(partnerId: Long, expectedId: Long)
-        = inboundRepository.findInboundActualDetail(partnerId, expectedId)
+    fun getInboundActualDetail(partnerId: Long, expectedId: Long) : GetInboundDetailModel {
+        val inboundExpectedDetail =
+            inboundExpectedDetailService.getInboundExpectedDetail(expectedId)
+        return GetInboundDetailModel.toDto(inboundExpectedDetail)
+    }
+
 
     @Transactional
-    fun cancelInbound(inboundId : Long){
+    fun cancelInbound(inboundId: Long) {
         val inbound = inboundRepository.findById(inboundId).get()
 
         inbound.status.isCancelPossiible()
 
-        val receivingPlanId = inbound.registrationId?: throw BaseRuntimeException(errorMessage = "입고예정등록번호를 찾을 수 없습니다")
+        val receivingPlanId = inbound.registrationId
+            ?: throw BaseRuntimeException(errorMessage = "입고예정등록번호를 찾을 수 없습니다")
 
         nosnosClientService.cancelInbound(inbound.partnerId, receivingPlanId)
 
