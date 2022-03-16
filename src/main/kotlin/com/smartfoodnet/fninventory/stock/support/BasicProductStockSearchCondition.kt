4498b03d358eed808d5ae090affb205a8420226a
leon <yohwan.cho@smartfoodnet.com>
2022-03-16 18:42:00 +0900

    재고조회 수정 스웨거 수정 총재고수량 수량

--- src/main/kotlin/com/smartfoodnet/fninventory/stock/support/BasicProductStockSearchCondition.kt
+++ src/main/kotlin/com/smartfoodnet/fninventory/stock/support/BasicProductStockSearchCondition.kt
@@ -4,12 +4,13 @@ import com.querydsl.core.BooleanBuilder
 import com.querydsl.core.types.Predicate
 import com.smartfoodnet.common.model.request.PredicateSearchCondition
 import com.smartfoodnet.fnproduct.product.entity.QBasicProduct.basicProduct
+import com.smartfoodnet.fninventory.stock.support.BasicProductStockSearchCondition.SearchType.*
 import io.swagger.annotations.ApiModelProperty
 
 class BasicProductStockSearchCondition(
     @ApiModelProperty(hidden = true)
     var partnerId: Long? = null,
-
+/*
     @ApiModelProperty(value = "기본상품명")
     var basicProductName: String? = null,
 
@@ -18,19 +19,42 @@ class BasicProductStockSearchCondition(
 
     @ApiModelProperty(value = "상품바코드")
     var barcode: String? = null,
-
+*/
     @ApiModelProperty(value = "유통기한관리여부")
     var expirationDateManagementYn: String? = null,
 ) : PredicateSearchCondition() {
     override fun assemblePredicate(predicate: BooleanBuilder): Predicate {
         return predicate.orAllOf(
             eqPartnerId(partnerId),
+            /*
             likeBasicProductName(basicProductName),
             eqBasicProductCode(basicProductCode),
             eqBarcode(barcode),
+             */
+            searchType?.let { toPredicate(it) },
             eqExpirationDateManagementYn(expirationDateManagementYn)
         )
     }
+
+    @ApiModelProperty(
+        value = "상품별검색 (NAME:기본상품명/CODE:기본상품코드/BARCODE:상품바코드)",
+        allowableValues = "NAME,CODE,BARCODE"
+    )
+    var searchType: SearchType? = null
+
+    @ApiModelProperty(value = "검색 키워드")
+    var searchKeyword: String? = null
+
+    enum class SearchType {
+        NAME, CODE, BARCODE
+    }
+
+    private fun toPredicate(searchType: SearchType) =
+        when (searchType) {
+            NAME -> likeBasicProductName(searchKeyword)
+            CODE -> eqBasicProductCode(searchKeyword)
+            BARCODE -> eqBarcode(searchKeyword)
+        }
 
     private fun eqPartnerId(partnerId: Long?) = partnerId?.let { basicProduct.partnerId.eq(it) }
 
