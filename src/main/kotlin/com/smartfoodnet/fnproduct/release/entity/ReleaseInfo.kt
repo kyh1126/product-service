995a6da2589d6984e8a728e9454c937880ef5e02
jenny <yunhee.kim@smartfoodnet.com>
2022-03-16 15:15:52 +0900

    feat: 릴리즈-주문 구조 변경 (ReleaseOrderMapping 엔티티 추가)

--- src/main/kotlin/com/smartfoodnet/fnproduct/release/entity/ReleaseInfo.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/release/entity/ReleaseInfo.kt
@@ -1,10 +1,8 @@
 package com.smartfoodnet.fnproduct.release.entity
 
-import com.smartfoodnet.fnproduct.order.entity.ConfirmOrder
+import com.smartfoodnet.common.entity.SimpleBaseEntity
 import com.smartfoodnet.fnproduct.release.model.vo.ReleaseStatus
 import com.smartfoodnet.fnproduct.release.model.vo.ShippingCodeStatus
-import org.hibernate.annotations.CreationTimestamp
-import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 import javax.persistence.*
 
@@ -16,15 +14,11 @@ class ReleaseInfo(
     @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
     var id: Long? = null,
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "confirm_order_id", columnDefinition = "BIGINT UNSIGNED")
-    var confirmOrder: ConfirmOrder? = null,
-
     @Column(name = "order_id")
-    var orderId: Long? = null,
+    var orderId: Long,
 
     @Column(name = "order_code")
-    var orderCode: String? = null,
+    var orderCode: String,
 
     @Column(name = "release_id")
     var releaseId: Long? = null,
@@ -55,11 +49,6 @@ class ReleaseInfo(
     @Column(name = "delivery_completed_at")
     var deliveryCompletedAt: LocalDateTime? = null,
 
-    @CreationTimestamp
-    @Column(name = "created_at")
-    var createdAt: LocalDateTime? = null,
-
-    @UpdateTimestamp
-    @Column(name = "updated_at")
-    var updatedAt: LocalDateTime? = null
-)
+    @OneToMany(mappedBy = "releaseInfo", cascade = [CascadeType.PERSIST])
+    var releaseOrderMappings: MutableSet<ReleaseOrderMapping> = LinkedHashSet()
+) : SimpleBaseEntity()
