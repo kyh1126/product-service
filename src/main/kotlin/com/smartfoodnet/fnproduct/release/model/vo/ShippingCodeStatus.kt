d90997eb691f7ade9b44c252616c21f3a01797ab
jenny <yunhee.kim@smartfoodnet.com>
2022-03-31 13:52:56 +0900

    feat: 특정 enum 컬렉션 내에서 enum 값을 찾아내는 로직 추가 - 효율성 측면에서 Set -> EnumSet 타입으로 변경

--- src/main/kotlin/com/smartfoodnet/fnproduct/release/model/vo/ShippingCodeStatus.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/release/model/vo/ShippingCodeStatus.kt
@@ -1,7 +1,19 @@
 package com.smartfoodnet.fnproduct.release.model.vo
 
-enum class ShippingCodeStatus(val description: String) {
-    UNREGISTERED("송장등록전"),
-    REGISTERED("성공"),
-    REGISTRATION_FAILED("송장등록실패")
+import com.smartfoodnet.fnproduct.order.vo.OrderUploadType
+import java.util.*
+
+enum class ShippingCodeStatus(val description: String, val uploadType: OrderUploadType) {
+    REGISTERED("자동송장등록성공", OrderUploadType.API),
+    REGISTER_FAILED("자동송장등록실패", OrderUploadType.API),
+    BEFORE_REGISTER("자동송장등록전", OrderUploadType.API),
+    EXCEL_REGISTER("수기등록", OrderUploadType.UPLOAD);
+
+    companion object {
+        private val INITIAL_STATUSES = EnumSet.of(BEFORE_REGISTER, EXCEL_REGISTER)
+
+        fun getInitialStatus(uploadType: OrderUploadType): ShippingCodeStatus? {
+            return INITIAL_STATUSES.firstOrNull { it.uploadType == uploadType }
+        }
+    }
 }
