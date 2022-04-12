5af513e1a2766bc2a44669e5bcb2955bf53ed0c3
woo.noh <wooyoung.noh@smartfoodnet.com>
2022-04-12 16:07:32 +0900

    fix: localTestCode deleted

--- src/test/kotlin/com/smartfoodnet/sample/CodeTest.kt
+++ src/test/kotlin/com/smartfoodnet/sample/CodeTest.kt
@@ -1,19 +1,13 @@
 package com.smartfoodnet.sample
 
-import com.smartfoodnet.fnproduct.claim.ClaimService
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @ActiveProfiles("local")
 class CodeTest {
-    @Autowired
-    lateinit var claimService: ClaimService
-
     @Test
     fun test() {
-        claimService.getReturnInfo()
     }
 }
