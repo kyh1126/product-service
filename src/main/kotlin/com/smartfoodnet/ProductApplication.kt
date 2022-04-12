d8b637f44e716af3684e7c836116a898db52d66e
leon <yohwan.cho@smartfoodnet.com>
2022-04-12 20:06:56 +0900

    서비스 순단 조치

--- src/main/kotlin/com/smartfoodnet/ProductApplication.kt
+++ src/main/kotlin/com/smartfoodnet/ProductApplication.kt
@@ -1,12 +1,26 @@
 package com.smartfoodnet
 
+import com.smartfoodnet.common.model.response.CommonResponse
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @EnableFeignClients
 @SpringBootApplication
-class ProductApplication
+class ProductApplication(){
+
+    @RestController
+    @RequestMapping("/health")
+    class HealthCheckController {
+        @GetMapping
+        fun healthCheck(): CommonResponse<String> {
+            return CommonResponse("fn-product ok")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ProductApplication>(*args)
