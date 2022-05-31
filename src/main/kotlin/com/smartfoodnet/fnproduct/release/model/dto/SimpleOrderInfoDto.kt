f5c34596f7ffa136109ee4099e7503f72a725a10
jenny <yunhee.kim@smartfoodnet.com>
2022-05-31 09:43:55 +0900

    feat: 출고정보 상세 조회 API 필드 추가 - 쇼핑몰 이름 - 쇼핑몰 아이콘 - 묶음번호

--- src/main/kotlin/com/smartfoodnet/fnproduct/release/model/dto/SimpleOrderInfoDto.kt
+++ src/main/kotlin/com/smartfoodnet/fnproduct/release/model/dto/SimpleOrderInfoDto.kt
@@ -16,15 +16,33 @@ data class SimpleOrderInfoDto(
     val receiverModel: ReceiverModel,
 
     @ApiModelProperty(value = "주문번호")
-    val orderNumbers: List<String>
+    val orderNumbers: List<String>,
+
+    @ApiModelProperty(value = "쇼핑몰 이름")
+    var storeName: String,
+
+    @ApiModelProperty(value = "쇼핑몰 아이콘")
+    var storeIcon: String? = null,
+
+    @ApiModelProperty(value = "묶음번호")
+    var bundleNumber: String,
 ) {
     companion object {
-        fun from(orderId: Long, orderCode: String, collectedOrders: List<CollectedOrder>): SimpleOrderInfoDto {
+        fun from(
+            orderId: Long,
+            orderCode: String,
+            collectedOrders: List<CollectedOrder>
+        ): SimpleOrderInfoDto {
+            val firstCollectedOrder = collectedOrders.first()
+
             return SimpleOrderInfoDto(
                 orderId = orderId,
                 orderCode = orderCode,
-                receiverModel = collectedOrders.first().receiver.run(ReceiverModel::from),
-                orderNumbers = collectedOrders.map { it.orderNumber }
+                receiverModel = firstCollectedOrder.receiver.run(ReceiverModel::from),
+                orderNumbers = collectedOrders.map { it.orderNumber },
+                storeName = firstCollectedOrder.storeName,
+                storeIcon = firstCollectedOrder.storeIcon,
+                bundleNumber = firstCollectedOrder.bundleNumber
             )
         }
     }
